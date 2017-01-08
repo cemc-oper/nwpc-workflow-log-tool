e46286ce4c09e721e4cf2cc23c59780b07610150
perillaroc <perillaroc@gmail.com>
2017-01-08 16:47:40 +0800

    move chart data generation code into a new script.

--- nwpc_log_processor/run_time_line/scripts/process_time_line.sh
+++ nwpc_log_processor/run_time_line/scripts/process_time_line.sh
@@ -6,13 +6,7 @@ export PYTHONPATH=${BASE_DIR}:$PYTHONPATH
 
 query_date=$1
 
-python ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_op --date=${query_date} --save-to-db
-python ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_qu --date=${query_date} --save-to-db
-python ${WORK_DIR}/time_line_processor.py -o nwp_xp -r eps_nwpc_qu --date=${query_date} --save-to-db
-python ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_pd --date=${query_date} --save-to-db
-
-python ${WORK_DIR}/time_line_chart_data_generator.py \
-    --config=${WORK_DIR}/conf/chart_data.schema.json \
-    --date=${query_date} \
-    --print \
-    --output-file=${WORK_DIR}/output/${query_date}.data.json
+python3 ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_op --date=${query_date} --save-to-db
+python3 ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_qu --date=${query_date} --save-to-db
+python3 ${WORK_DIR}/time_line_processor.py -o nwp_xp -r eps_nwpc_qu --date=${query_date} --save-to-db
+python3 ${WORK_DIR}/time_line_processor.py -o nwp_xp -r nwpc_pd --date=${query_date} --save-to-db
