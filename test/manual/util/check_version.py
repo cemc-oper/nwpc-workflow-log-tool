b577beffa19aafb4cd738673828b4e80be87f69a
perillaroc <perillaroc@gmail.com>
2017-01-08 09:59:43 +0800

    add get_version function.

---
+++ test/manual/util/check_version.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from nwpc_log_util.version_util import get_version
+
+if __name__ == "__main__":
+    file_path = "/vagrant_data/sms-log-sample/nwp/cma20n03.sms.log"
+    with open(file_path) as f:
+        first_line = f.readline().strip()
+        print(first_line)
+        version = get_version('nwp_xp', 'nwpc_op', first_line)
+        print(version)
