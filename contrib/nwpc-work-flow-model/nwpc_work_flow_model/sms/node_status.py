e90f76c4e8b393e983d940edf23c741a9ababa64
perillaroc <perillaroc@gmail.com>
2017-01-11 15:00:37 +0800

    delete subtree.

--- contrib/nwpc-work-flow-model/nwpc_work_flow_model/sms/node_status.py
+++
@@ -1,5 +0,0 @@
-class NodeStatus(object):
-    def __init__(self, node_type, node_path, node_date):
-        self.node_type = node_type
-        self.node_path = node_path
-        self.node_date = node_date
