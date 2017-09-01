87c814564bdbca2afc65086f644d2cd5a12d36e9
perillaroc <perillaroc@gmail.com>
2017-09-01 14:51:17 +0800

    use vendor instead of contrib.

--- contrib/nwpc-work-flow-model/nwpc_work_flow_model/sms/__init__.py
+++
@@ -1,4 +0,0 @@
-from .node import Node, NodeType
-from .bunch import Bunch
-from .node_status import NodeStatus
-from .visitor import NodeVisitor, SimplePrintVisitor, pre_order_travel, ErrorStatusTaskVisitor
