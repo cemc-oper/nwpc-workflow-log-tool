4f9634b8043b2d033dc7872ee4867da8463c9297
perillaroc <perillaroc@gmail.com>
2018-10-31 21:21:41 +0800

    :sparkles: parse client command in ecflow log.

--- nwpc-workflow-log-model/nwpc_workflow_log_model/rmdb/ecflow/record.py
+++ nwpc-workflow-log-model/nwpc_workflow_log_model/rmdb/ecflow/record.py
@@ -42,12 +42,13 @@ class EcflowRecord(object):
             record.parse_status_record(line[start_pos:])
         elif line[start_pos: start_pos+2] == "--":
             record.command_type = "client"
-            # print("[client command]", line)
+            start_pos += 2
+            record.parse_client_record(line[start_pos:])
         elif line[start_pos: start_pos+4] == "chd:":
             # child
             record.command_type = "child"
             start_pos += 4
-            record.parse_client_record(line[start_pos:])
+            record.parse_child_record(line[start_pos:])
         elif line[start_pos: start_pos+4] == "svr:":
             # server
             # print("[server command]", line)
@@ -55,82 +56,8 @@ class EcflowRecord(object):
         else:
             # not supported
             print("[not supported]", line)
+
         return record
-
-        if self.record_command == 'alter':
-            start_pos = end_pos+1
-            pos = line.find(' [', start_pos)
-            if pos != -1:
-                self.record_fullname = line[start_pos:pos]
-                start_pos = pos + 2
-                end_pos = line.find('] ', start_pos)
-                if end_pos != -1:
-                    self.record_additional_information = line[start_pos:end_pos]
-
-        elif self.record_command == 'meter':
-            if self.record_type != 'ERR':
-                start_pos = end_pos + 1
-                end_pos = line.find(' ', start_pos)
-                self.record_fullname = line[start_pos:end_pos]
-                start_pos = end_pos + 4
-                self.record_additional_information = line[start_pos:]
-            else:
-                start_pos = end_pos + 1
-                if line[start_pos] == "/":
-                    # ERR:[12:52:00 19.5.2014] meter:/gmf_gsi_v1r5/T639/06/dasrefresh:WaitingMins: 55 out of range [0 - 40]
-                    end_pos = line.find(' ', start_pos)
-                    if end_pos != -1 and line[end_pos-1] == ":":
-                        self.record_fullname = line[start_pos: end_pos-1]
-                        self.record_additional_information = line[end_pos+1:]
-                else:
-                    # ERR:[03:41:58 10.7.2014] meter:WaitingMins for /grapes_meso_v4_0/cold/00/pre_data/obs_get/aob_get:the node was not found:
-                    pass
-
-        elif self.record_command in ['begin', 'autorepeat date']:
-            start_pos = end_pos + 1
-            end_pos = line.find(' ', start_pos)
-            self.record_fullname = line[start_pos: end_pos]
-
-        elif self.record_command == 'force' or self.record_command == 'force(recursively)':
-            start_pos = end_pos + 1
-            end_pos = line.find(' ', start_pos)
-            self.record_fullname = line[start_pos:end_pos]
-            if line[end_pos:end_pos+4] == " to ":
-                start_pos = end_pos + 4
-                end_pos = line.find(' ', start_pos)
-                self.record_additional_information = line[start_pos:end_pos]
-
-        elif self.record_command == 'delete':
-            start_pos = end_pos + 1
-            end_pos = line.find(' ', start_pos)
-            self.record_fullname = line[start_pos:end_pos]
-            start_pos = end_pos + 1
-            end_pos = line.find(' ', start_pos)
-            self.record_additional_information = line[start_pos:end_pos]
-
-        elif self.record_command in ['set', 'clear']:
-            start_pos = end_pos + 1
-            end_pos = line.find(':', start_pos)
-            if end_pos != -1:
-                self.record_fullname = line[start_pos: end_pos]
-                self.record_additional_information = line[end_pos+1:]
-
-        # WAR:[23:37:08 16.8.2015] requeue:/gmf_grapes_v1423/grapes_global/12/post/postp_084 from aborted
-        # MSG:[23:37:08 16.8.2015] requeue:user nwp@5986333:/gmf_grapes_v1423/grapes_global/12/post/postp_084
-        elif self.record_command == 'requeue':
-            start_pos = end_pos + 1
-            if self.record_type == "WAR":
-                end_pos = line.find(' ', start_pos)
-                if end_pos != -1:
-                    self.record_fullname = line[start_pos: end_pos]
-                    start_pos = end_pos + 1
-                    self.record_additional_information = line[start_pos:]
-            elif self.record_type == "MSG":
-                end_pos = line.find(':', start_pos)
-                if end_pos != -1:
-                    self.record_additional_information = line[start_pos: end_pos]
-                    start_pos = end_pos + 1
-                    self.record_fullname = line[start_pos:]
 
     def parse_status_record(self, status_line):
         """
@@ -164,37 +91,127 @@ class EcflowRecord(object):
                 self.command = command
                 print("[ERROR] status record: command not supported =>", self.log_record)
 
-    def parse_client_record(self, client_line):
+    def parse_child_record(self, child_line):
         start_pos = 0
-        end_pos = client_line.find(" ", start_pos)
+        end_pos = child_line.find(" ", start_pos)
         if end_pos == -1:
-            print("[ERROR] client record: command not found =>", self.log_record)
+            print("[ERROR] child record: command not found =>", self.log_record)
             return
-        command = client_line[start_pos:end_pos]
+        command = child_line[start_pos:end_pos]
 
         if command in ('init', 'complete', 'abort'):
             self.command = command
             start_pos = end_pos + 2
-            end_pos = client_line.find(' ', start_pos)
+            end_pos = child_line.find(' ', start_pos)
             if end_pos == -1:
                 # MSG:[08:17:04 29.6.2018] chd:complete /gmf_grapes_025L60_v2.2_post/18/typhoon/post/tc_post
-                self.node_path = client_line[start_pos:].strip()
+                self.node_path = child_line[start_pos:].strip()
             else:
                 # MSG:[12:22:53 19.10.2018] chd:abort /3km_post/06/3km_togrib2/grib2WORK/030/after_data2grib2_030  trap
-                self.node_path = client_line[start_pos:end_pos]
-                self.additional_information = client_line[end_pos + 1:]
+                self.node_path = child_line[start_pos:end_pos]
+                self.additional_information = child_line[end_pos + 1:]
         elif command in ('meter', 'label', 'event'):
             self.command = command
             # MSG:[09:24:06 29.6.2018] chd:event transmissiondone /gmf_grapes_025L60_v2.2_post/00/tograph/base/015/AN_AEA/QFLXDIV_P700_AN_AEA_sep_015
             self.command = command
             start_pos = end_pos + 1
-            line = client_line[start_pos:]
+            line = child_line[start_pos:]
             node_path_start_pos = line.rfind(' ')
             if node_path_start_pos != -1:
                 self.node_path = line[node_path_start_pos+1:]
                 self.additional_information = line[:node_path_start_pos]
             else:
-                print("[ERROR] client record: parse error =>", self.log_record)
+                print("[ERROR] child record: parse error =>", self.log_record)
+        else:
+            self.command = command
+            print("[ERROR] child record: command not supported =>", self.log_record)
+
+    def parse_client_record(self, child_line):
+        start_pos = 0
+        end_pos = child_line.find(" ", start_pos)
+        if end_pos == -1:
+            print("[ERROR] client record: command not found =>", self.log_record)
+            return
+        command = child_line[start_pos:end_pos]
+
+        if command == 'requeue':
+            self.command = command
+            start_pos = end_pos + 1
+            tokens = child_line[start_pos:].split()
+            if len(tokens) == 3:
+                requeue_option = tokens[0]
+                node_path = tokens[1]
+                user = tokens[2]
+                self.node_path = node_path
+                self.additional_information = requeue_option + ' ' + user
+            else:
+                print("[ERROR] client record: requeue parse error =>", self.log_record)
+                return
+        elif command in ('alter', 'free-dep', 'kill', 'delete', 'suspend', 'resume', 'run', 'status'):
+            self.command = command
+            start_pos = end_pos + 1
+            tokens = child_line[start_pos:].split()
+            user = tokens[-1]
+            node_path = tokens[-2]
+            self.node_path = node_path
+            self.additional_information = ' '.join(tokens[:-2]) + ' ' + user
+        elif command.startswith('force='):
+            self.command = 'force'
+            start_pos = end_pos + 1
+            tokens = child_line[start_pos:].split()
+            node_path = tokens[-2]
+            user = tokens[-1]
+            self.node_path = node_path
+            self.additional_information = ' '.join(tokens[-2:]) + ' ' + user
+        elif command.startswith('file='):
+            self.command = 'file'
+            node_path = command[5:]
+            self.node_path = node_path
+            start_pos = end_pos + 1
+            self.additional_information = child_line[start_pos:]
+        elif command.startswith('load='):
+            self.command = 'load'
+            node_path = command[5:]
+            self.node_path = node_path
+            start_pos = end_pos + 1
+            self.additional_information = child_line[start_pos:]
+        elif command.startswith('begin='):
+            self.command = 'begin'
+            node_path = command[6:]
+            self.node_path = node_path
+            start_pos = end_pos + 1
+            self.additional_information = child_line[start_pos:]
+        elif command.startswith('replace='):
+            self.command = 'replace'
+            node_path = command[5:]
+            self.node_path = node_path
+            start_pos = end_pos + 1
+            self.additional_information = child_line[start_pos:]
+        elif command.startswith('order='):
+            self.command = 'order'
+            node_path = command[6:]
+            self.node_path = node_path
+            start_pos = end_pos + 1
+            self.additional_information = child_line[start_pos:]
+        elif command in ('restart', 'suites', 'stats', 'edit_history',
+                         'zombie_get', 'server_version', 'ping', 'check_pt'):
+            self.command = command
+        elif command.startswith('sync_full=') or \
+                command.startswith('news=') or \
+                command.startswith('sync=') or \
+                command.startswith('edit_script=') or \
+                command.startswith('zombie_fail=') or \
+                command.startswith('zombie_kill=') or \
+                command.startswith('zombie_fob=') or \
+                command.startswith('zombie_adopt=') or \
+                command.startswith('zombie_remove=') or \
+                command.startswith('log=') or \
+                command.startswith('halt=') or \
+                command.startswith('terminate=') or \
+                command.startswith('order=') or \
+                command.startswith('ch_register=') or \
+                command.startswith('ch_drop='):
+            self.command = command[:command.find('=')]
         else:
             self.command = command
             print("[ERROR] client record: command not supported =>", self.log_record)
