710e9eb48108e5e5ccfcdd1ec965c601a812bbc0
perillaroc <perillaroc@gmail.com>
2019-07-24 16:18:50 +0800

    :sparkles: update cpu calculator to use processor config file.

--- nwpc-workflow-log-processor/nwpc_workflow_log_processor/rmdb/run_time_line/cpu_calculator.py
+++ nwpc-workflow-log-processor/nwpc_workflow_log_processor/rmdb/run_time_line/cpu_calculator.py
@@ -1,23 +1,111 @@
 # coding=utf-8
-from __future__ import absolute_import
-import argparse
 import datetime
 import csv
+import pathlib
+
+import click
+import yaml
 from pymongo import MongoClient
 
-from nwpc_workflow_log_processor.rmdb.run_time_line import MONGODB_HOST, MONGODB_PORT
-
-from nwpc_workflow_log_processor.rmdb.run_time_line.time_line_chart_data_generator import load_schema
-
-mongodb_client = MongoClient(MONGODB_HOST, MONGODB_PORT)
-smslog_mongodb = mongodb_client.smslog
-daily_tree_status_collection = smslog_mongodb.daily_tree_status_collection
-daily_repo_time_line_collection = smslog_mongodb.daily_repo_time_line_collection
-
-repo_time_line_collection = smslog_mongodb.daily_repo_time_line_collection
+from nwpc_workflow_log_processor.rmdb.run_time_line.config import load_processor_config
 
 
-def calculate_cpu_for_repo(owner, repo, query_date):
+@click.group()
+def cli():
+    """
+    Calculate cpu
+    """
+    pass
+
+
+@cli.command('suite')
+@click.option("-c", "--config", "config_file_path", help="config file path", required=True)
+@click.option("-o", "--owner", help="owner name", required=True)
+@click.option("-r", "--repo", help="repo name", required=True)
+@click.option("-d", "--date", help="query date (%Y-%m-%d)", required=True)
+@click.option("--output-file", help="output file path")
+@click.option("-p", "--print", "print_flag", is_flag=True, help="print result to console.")
+def suite(config_file_path, owner, repo, date, output_file, print_flag):
+    """
+    collect sms status from sms server.
+    """
+    print('user name: {owner}'.format(owner=owner))
+    print('repo name: {repo}'.format(repo=repo))
+
+    query_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    print('query date: {query_date}'.format(query_date=query_date))
+
+    config = load_processor_config(config_file_path)
+
+    suite_rows = calculate_cpu_for_repo(config, owner, repo, query_date)
+    if suite_rows is None:
+        print("Fatal Error: suite rows is None")
+        return
+
+    time_array = [(query_date + datetime.timedelta(minutes=i)).strftime("%H:%M") for i in range(0, 60 * 24)]
+    title_row = ["suite"]
+    title_row.extend(time_array)
+
+    print(title_row)
+    for a_suite in suite_rows:
+        print(a_suite)
+
+    with open('output/' + owner + '.' + repo + '.' + date + '.' + '.csv', 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(title_row)
+        for a_row in suite_rows:
+            csv_writer.writerow(a_row)
+    return
+
+
+@cli.command('system')
+@click.option("-c", "--config", "config_file_path", help="config file path", required=True)
+@click.option("-s", "--schema", "schema_file_path", help="schema file path", required=True)
+@click.option("-d", "--date", help="query date (%Y-%m-%d)", required=True)
+@click.option("--output-file", help="output file path")
+@click.option("-p", "--print", "print_flag", is_flag=True)
+def system_handler(config_file_path, schema_file_path, date, output_file, print_flag):
+    print('schema file path:', schema_file_path)
+    query_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    print('query date: {query_date}'.format(query_date=query_date))
+
+    config = load_processor_config(config_file_path)
+
+    schema = load_schema(schema_file_path)
+    if schema is None:
+        print('Fatal error: load schema file failed')
+
+    system_cpu_usage_rows = []
+
+    for a_server in schema['series']:
+        owner_name = a_server['owner']
+        repo_name = a_server['repo']
+        server_suite_cpu_usage = calculate_cpu_for_repo(config, owner_name, repo_name, query_date)
+        system_cpu_usage_rows.extend(server_suite_cpu_usage)
+
+    time_array = [(query_date + datetime.timedelta(minutes=i)).strftime("%H:%M") for i in range(0, 60 * 24)]
+    title_row = ["suite"]
+    title_row.extend(time_array)
+
+    print(title_row)
+    for a_suite in system_cpu_usage_rows:
+        print(a_suite)
+
+    if output_file:
+        with open(output_file, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(title_row)
+            for a_row in system_cpu_usage_rows:
+                csv_writer.writerow(a_row)
+
+
+def calculate_cpu_for_repo(config, owner, repo, query_date):
+    statistic_dbms_config = config['datastore']['statistic_dbms']
+    mongodb_client = MongoClient(statistic_dbms_config['host'], statistic_dbms_config['port'])
+    mongodb_database = mongodb_client[statistic_dbms_config['database']]
+
+    daily_repo_time_line_collection = mongodb_database.daily_repo_time_line_collection
+
     # get data
     time_line_key = {
         'owner': owner,
@@ -78,107 +166,11 @@ def calculate_cpu_for_repo(owner, repo, query_date):
     return suite_rows
 
 
-def repo_handler(args):
-    owner_name = args.owner
-    print('user name: {owner_name}'.format(owner_name=owner_name))
-
-    repo_name = args.repo
-    print('repo name: {repo_name}'.format(repo_name=repo_name))
-
-    query_date = datetime.datetime.strptime(args.date, "%Y-%m-%d")
-    print('query date: {query_date}'.format(query_date=query_date))
-
-    suite_rows = calculate_cpu_for_repo(owner_name, repo_name, query_date)
-    if suite_rows is None:
-        print("Fatal Error: suite rows is None")
-        return
-
-    time_array = [(query_date + datetime.timedelta(minutes=i)).strftime("%H:%M") for i in range(0, 60 * 24)]
-    title_row = ["suite"]
-    title_row.extend(time_array)
-
-    print(title_row)
-    for a_suite in suite_rows:
-        print(a_suite)
-
-    with open('output/' + owner_name + '.' + repo_name + '.' + args.date + '.' + '.csv', 'w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(title_row)
-        for a_row in suite_rows:
-            csv_writer.writerow(a_row)
-    return
-
-
-def system_handler(args):
-    schema_file_path = args.schema
-    print('schema file path:', schema_file_path)
-    query_date = datetime.datetime.strptime(args.date, "%Y-%m-%d")
-    print('query date: {query_date}'.format(query_date=query_date))
-
-    schema = load_schema(schema_file_path)
-    if schema is None:
-        print('Fatal error: load schema file failed')
-
-    system_cpu_usage_rows = []
-
-    for a_server in schema['series']:
-        owner_name = a_server['owner']
-        repo_name = a_server['repo']
-        server_suite_cpu_usage = calculate_cpu_for_repo(owner_name, repo_name, query_date)
-        system_cpu_usage_rows.extend(server_suite_cpu_usage)
-
-    time_array = [(query_date + datetime.timedelta(minutes=i)).strftime("%H:%M") for i in range(0, 60 * 24)]
-    title_row = ["suite"]
-    title_row.extend(time_array)
-
-    print(title_row)
-    for a_suite in system_cpu_usage_rows:
-        print(a_suite)
-
-    if args.output_file:
-        with open(args.output_file, 'w') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow(title_row)
-            for a_row in system_cpu_usage_rows:
-                csv_writer.writerow(a_row)
-
-
-def main():
-    start_time = datetime.datetime.now()
-
-    # argument parser
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="""\
-DESCRIPTION
-    Calculate cpu.""")
-
-    sub_parsers = parser.add_subparsers(title="sub commands", dest="sub_command")
-
-    suite_parser = sub_parsers.add_parser('suite', description="collect sms status from sms server.")
-    suite_parser.add_argument("-o", "--owner", help="owner name", required=True)
-    suite_parser.add_argument("-r", "--repo", help="repo name", required=True)
-    suite_parser.add_argument("-d", "--date", help="query date (%Y-%m-%d)", required=True)
-    suite_parser.add_argument("--output-file", help="output file path")
-    suite_parser.add_argument("-p", "--print", dest='print_flag', help="print result to console.", action='store_true')
-
-    server_parser = sub_parsers.add_parser('system', description="collect sms status from sms server.")
-    server_parser.add_argument("-s", "--schema", help="schema file path", required=True)
-    server_parser.add_argument("-d", "--date", help="query date (%Y-%m-%d)", required=True)
-    server_parser.add_argument("--output-file", help="output file path")
-    server_parser.add_argument("-p", "--print", dest='print_flag', help="print result to console.", action='store_true')
-
-    args = parser.parse_args()
-
-    if args.sub_command == "suite":
-        repo_handler(args)
-    elif args.sub_command == "system":
-        system_handler(args)
-
-    end_time = datetime.datetime.now()
-    print(end_time - start_time)
-    return
+def load_schema(schema_path):
+    with open(schema_path, 'r') as config_file:
+        schema = yaml.safe_load(config_file)
+        return schema
 
 
 if __name__ == "__main__":
-    main()
+    cli()
