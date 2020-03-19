a98a7bb012e4c1118d09a2f0a51a5eb874a8b077
perillaroc <perillaroc@gmail.com>
2020-03-19 17:05:03 +0800

    :recycle: add SituationCalculator and use it for task and family node.

---
+++ nwpc_workflow_log_tool/situation/situation_calculator.py
@@ -0,0 +1,69 @@
+import typing
+import datetime
+
+from loguru import logger
+import pandas as pd
+
+from nwpc_workflow_log_model.log_record.ecflow import StatusLogRecord
+from nwpc_workflow_log_model.log_record.ecflow.status_record import StatusChangeEntry
+
+from nwpc_workflow_log_tool.util import generate_in_date_range
+from .situation_record import SituationRecord
+
+
+class SituationCalculator(object):
+    def __init__(
+            self,
+            dfa_engine,
+            stop_states: typing.Tuple,
+            dfa_kwargs: dict = None,
+    ):
+        self._dfa_engine = dfa_engine
+        self._stop_states = stop_states
+        self._dfa_kwargs = dfa_kwargs
+        if self._dfa_kwargs is None:
+            self._dfa_kwargs = dict()
+
+    def get_situations(
+            self,
+            records: typing.List,
+            node_path: str,
+            start_date: datetime.datetime,
+            end_date: datetime.datetime,
+    ) -> typing.List[SituationRecord]:
+        logger.info("Finding StatusLogRecord for {}", node_path)
+        record_list = []
+        for record in records:
+            if record.node_path == node_path and isinstance(record, StatusLogRecord):
+                record_list.append(record)
+
+        logger.info("Calculating family node status change using DFA...")
+        situations = []
+        for current_date in pd.date_range(start=start_date, end=end_date, closed="left"):
+            filter_function = generate_in_date_range(current_date, current_date + pd.Timedelta(days=1))
+            current_records = list(filter(lambda x: filter_function(x), record_list))
+
+            status_changes = [StatusChangeEntry(r) for r in current_records]
+
+            dfa = self._dfa_engine(
+                name=current_date,
+                **self._dfa_kwargs,
+            )
+
+            for s in status_changes:
+                dfa.trigger(
+                    s.status.value,
+                    node_data=s,
+                )
+                if dfa.state in self._stop_states:
+                    break
+
+            situations.append(SituationRecord(
+                date=current_date,
+                state=dfa.state,
+                node_situation=dfa.node_situation,
+                records=current_records,
+            ))
+
+        logger.info("Calculating family node status change using DFA...Done")
+        return situations
--- nwpc_workflow_log_tool/situation/task_node.py
+++ nwpc_workflow_log_tool/situation/task_node.py
@@ -1,22 +1,18 @@
 import datetime
 import typing
 
-import pandas as pd
 from loguru import logger
 
 from nwpc_workflow_model.node_status import NodeStatus
-from nwpc_workflow_log_model.log_record.ecflow import StatusLogRecord
-from nwpc_workflow_log_model.log_record.ecflow.status_record import StatusChangeEntry
 from nwpc_workflow_log_model.analytics.task_status_change_dfa import (
     TaskStatusChangeDFA,
     TaskSituationType,
 )
 from nwpc_workflow_log_collector.ecflow.log_file_util import get_record_list
 
-from nwpc_workflow_log_tool.util import generate_in_date_range
 from nwpc_workflow_log_tool.presenter import StatusPresenter
 
-from .situation_record import SituationRecord
+from .situation_calculator import SituationCalculator
 
 
 def analytics_task_node_log_with_status(
@@ -37,7 +33,14 @@ def analytics_task_node_log_with_status(
     records = get_record_list(file_path, node_path, start_date, stop_date)
     logger.info(f"Getting log lines...Done, {len(records)} lines")
 
-    situations = get_task_node_situations(
+    calculator = SituationCalculator(
+        dfa_engine=TaskStatusChangeDFA,
+        stop_states=(
+            TaskSituationType.Complete,
+        )
+    )
+
+    situations = calculator.get_situations(
         records=records,
         node_path=node_path,
         start_date=start_date,
@@ -49,48 +52,3 @@ def analytics_task_node_log_with_status(
         target_state=TaskSituationType.Complete,
     )
     presenter.present(situations)
-
-
-def get_task_node_situations(
-        records: list,
-        node_path: str,
-        start_date: datetime.datetime,
-        end_date: datetime.datetime,
-) -> typing.List[SituationRecord]:
-    logger.info("Finding StatusLogRecord for {}", node_path)
-    record_list = []
-    for record in records:
-        if record.node_path == node_path and isinstance(record, StatusLogRecord):
-            record_list.append(record)
-
-    logger.info("Calculating task node status change using DFA...")
-    situations = []
-    for current_date in pd.date_range(start=start_date, end=end_date, closed="left"):
-        filter_function = generate_in_date_range(current_date, current_date + pd.Timedelta(days=1))
-        current_records = list(filter(lambda x: filter_function(x), record_list))
-
-        status_changes = [StatusChangeEntry(r) for r in current_records]
-
-        dfa = TaskStatusChangeDFA(
-            name=current_date,
-        )
-
-        for s in status_changes:
-            dfa.trigger(
-                s.status.value,
-                node_data=s,
-            )
-            if dfa.state in (
-                    TaskSituationType.Complete,
-            ):
-                break
-
-        situations.append(SituationRecord(
-            date=current_date,
-            state=dfa.state,
-            node_situation=dfa.node_situation,
-            records=current_records,
-        ))
-
-    logger.info("Calculating task node status change using DFA...Done")
-    return situations
